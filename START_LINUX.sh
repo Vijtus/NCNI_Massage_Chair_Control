76fe0274ba9be824b9c653edf34f07d9d3fac0cf
Wiktor Dębowski <wiktor.vijtus.debowski@protonmail.com>
2026-06-11 09:47:27 +0200

    Delete START_LINUX.sh

--- START_LINUX.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")/app" || exit 1
-
-if [ ! -x ".venv/bin/python" ]; then
-  echo "First-time setup. This may take a minute..."
-  python3 install.py || exit 1
-fi
-
-exec ".venv/bin/python" app.py "$@"
